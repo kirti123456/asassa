94afaf170965461f2fac0051e1e9daaf3f1547b8
kirti123456 <kirtibande2294@gmail.com>
2016-07-04 22:14:59 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="login.css">
+	</head>
+	<body >
+		<form method="post" action="check1.php">
+
+		<div id="t_box">
+
+		<img src="i2.png" alt="oopsss" style="width:150px;height:150px;">
+
+		<input placeholder="username" name="user" style="margin-top:40px;" type= "text" class="un" >
+
+		<input placeholder="password" name="pwd" type= "password" class="pwd"><br/>
+		
+		<center><input type="submit" style="margin-top:25px;"  class="l_button" value="Login">
+
+		
+		
+		</div>
+		
+		</form>
+	</body>
+
+
+</html>
